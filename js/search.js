d17a866a2402d7f65971d4393b2d1be9e2e46adf
Ivan Bogachev <sfi0zy@gmail.com>
2016-12-10 14:36:52 +0300

    Add search form

---
+++ js/search.js
@@ -0,0 +1,65 @@
+function displaySearchResults(results, store) {
+    var searchResults = document.getElementById('search-results');
+
+    if (results.length) {
+        var appendString = '';
+
+        results.forEach(function(result) {
+            var item = store[result.ref];
+
+            appendString += '<li class=\'item\'><a href=' + item.url + '>' + item.title + '</a>';
+
+            if (item.content) {
+                appendString += '<p class=\'mui-p\'>' + item.content.substring(0, 150) + '...</p>';
+            }
+
+            appendString += '</li>';
+        });
+
+        searchResults.innerHTML = appendString;
+    } else {
+        searchResults.innerText = 'No results found';
+    }
+}
+
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1),
+        vars  = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+
+        if (pair[0] === variable) {
+            return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+        }
+    }
+}
+
+
+var searchTerm = getQueryVariable('query');
+
+
+if (searchTerm) {
+    document.getElementById('search-input').setAttribute('value', searchTerm);
+
+    var idx = lunr(function () {
+        this.field('id');
+        this.field('title', { boost: 10 });
+        this.field('category');
+        this.field('content');
+    });
+
+    for (var key in window.store) {
+        idx.add({
+            'id': key,
+            'title': window.store[key].title,
+            'category': window.store[key].category,
+            'content': window.store[key].content
+        });
+
+        var results = idx.search(searchTerm);
+
+        displaySearchResults(results, window.store);
+    }
+}
