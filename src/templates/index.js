e1d8e78b7fbc4256619f44ba81a197b423577ff5
Ivan Bogachev <sfi0zy@gmail.com>
2025-07-12 19:00:25 +0300

    Add the headers to the search results

--- src/templates/index.js
+++ src/templates/index.js
@@ -4,7 +4,7 @@ export default function $Index(config, posts, tag) {
     const selectedPosts = isHome ? posts
         : [...posts].filter((post) => post.tags.includes(tag));
 
-    let result = '';
+    let result = tag ? `<br><br><header><h2>Search results for "${tag}"</h2></header>` : '';
 
     const limit = isHome
         ? Math.max(0, selectedPosts.length - 1 - parseInt(config.homeIndexLimit, 10))
