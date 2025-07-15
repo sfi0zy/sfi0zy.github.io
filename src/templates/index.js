b612204b8296653490314d9e7b403b61538be426
Ivan Bogachev <sfi0zy@gmail.com>
2025-07-15 21:40:29 +0300

    Reduce the home index limit

--- src/templates/index.js
+++ src/templates/index.js
@@ -4,7 +4,7 @@ export default function $Index(config, posts, tag) {
     const selectedPosts = isHome ? posts
         : [...posts].filter((post) => post.tags.includes(tag));
 
-    let result = tag ? `<br><br><header><h2>Search results for "${tag}"</h2></header>` : '';
+    let result = isHome ? '' : `<br><br><header><h2>Search results for "${tag}"</h2></header>`;
 
     const limit = isHome
         ? Math.max(0, selectedPosts.length - 1 - parseInt(config.homeIndexLimit, 10))
@@ -12,6 +12,10 @@ export default function $Index(config, posts, tag) {
 
     for (let i = selectedPosts.length - 1; i >= limit; i--) {
         result = `${result} ${selectedPosts[i].compiled}`;
+    }
+
+    if (isHome) {
+        result = `${result}<br><br><div class='center'>Looking for something?</div>`;
     }
 
     return result;
