23e41a377356256fb8c46d9d9422b9ed02800a6c
Ivan Bogachev <sfi0zy@gmail.com>
2026-02-04 18:12:48 +0300

    Add CSS for printing

--- src/js/templates/footer-scripts.js
+++ src/js/templates/footer-scripts.js
@@ -1,18 +1,4 @@
 export default function $FooterScripts() {
-    return `<script>
-    const images = document.querySelectorAll('img');
-
-    images.forEach((image) => {
-        if (image.complete) {
-            image.setAttribute('data-lazy-loaded', true);
-        } else {
-            image.addEventListener('load', () => {
-                image.setAttribute('data-lazy-loaded', true);
-            });
-        }
-    });
-</script>
-
-<script src='https://cpwebassets.codepen.io/assets/embed/ei.js'></script>
+    return `<script src='https://cpwebassets.codepen.io/assets/embed/ei.js'></script>
 `;
 }
